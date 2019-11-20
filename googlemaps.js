e503a0589ac22ac4130ca6d2d81c24991f019fcb
Fernando Secchi <fernando@secchi.name>
2019-11-20 20:08:54 -0300

    Initial commit

---
+++ googlemaps.js
@@ -0,0 +1,86 @@
+
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.5880644, lng: -58.41187129999997},
+          zoom: 18
+        });
+        var card = document.getElementById('pac-card');
+        var input = document.getElementById('pac-input');
+        //var types = document.getElementById('type-selector');
+        // var strictBounds = document.getElementById('strict-bounds-selector');
+
+        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+
+        // Bind the map's bounds (viewport) property to the autocomplete object,
+        // so that the autocomplete requests use the current map bounds for the
+        // bounds option in the request.
+        autocomplete.bindTo('bounds', map);
+
+        // Set the data fields to return when the user selects a place.
+        autocomplete.setFields(
+            ['address_components', 'geometry', 'icon', 'name']);
+
+        var infowindow = new google.maps.InfoWindow();
+        var infowindowContent = document.getElementById('infowindow-content');
+        infowindow.setContent(infowindowContent);
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
+
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+          }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+            document.getElementById('calle').value = place.address_components[1].long_name;
+            document.getElementById('numero').value = place.address_components[0].long_name;
+            document.getElementById('barrio').value = place.address_components[2].long_name;
+            document.getElementById('comuna').value = place.address_components[3].long_name;
+            document.getElementById('provincia').value = place.address_components[4].long_name;
+            document.getElementById('pais').value = place.address_components[5].long_name;
+
+            var latitude = place.geometry.location.lat();
+            var longitude = place.geometry.location.lng();  
+
+            document.getElementById('latitud').value = latitude;
+            document.getElementById('longitud').value = longitude;
+     
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+
+          infowindowContent.children['place-icon'].src = place.icon;
+          infowindowContent.children['place-name'].textContent = place.name;
+          infowindowContent.children['place-address'].textContent = address;
+          infowindow.open(map, marker);
+        });
+
+        
+        
+      }
